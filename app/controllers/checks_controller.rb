016b235a36188cfc90faeec170f4580494dd8d5d
Mark Van Holstyn <mvanholstyn@gmail.com>
2017-07-19 22:53:40 -0400

    1 week

--- app/controllers/checks_controller.rb
+++ app/controllers/checks_controller.rb
@@ -1,6 +1,6 @@
 class ChecksController < ApplicationController
   def index
-    checks = Check.where("created_at >= ?", 15.days.ago)
+    checks = Check.where("created_at >= ?", 8.days.ago)
     render :json => {
       labels: checks.map(&:created_at),
       data: checks.map(&:up)
