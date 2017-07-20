ca18a2a2ea7920b80446b7d3d7b74da73db81033
Mark Van Holstyn <mvanholstyn@gmail.com>
2017-07-19 22:35:06 -0400

    limit checks to the last 30 days

--- app/controllers/checks_controller.rb
+++ app/controllers/checks_controller.rb
@@ -1,6 +1,6 @@
 class ChecksController < ApplicationController
   def index
-    checks = Check.all
+    checks = Check.where("created_at >= ?", 30.days.ago)
     render :json => {
       labels: checks.map(&:created_at),
       data: checks.map(&:up)
